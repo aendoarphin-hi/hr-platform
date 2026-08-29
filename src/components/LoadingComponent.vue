ca9278252df632a0fdc80942694f667c2a438946
Arhon <aronp2000@gmail.com>
2026-08-29 00:59:56 -0500

    center loader

--- src/components/LoadingComponent.vue
+++ src/components/LoadingComponent.vue
@@ -1,10 +1,11 @@
 <template>
   <!-- ===== loading state ===== -->
-  <div class="text-center py-5">
-    <div class="spinner-border text-primary" role="status">
-      <span class="visually-hidden">Loading...</span>
+  <div class="text-center py-5 h-100 d-flex flex-column">
+    <div class="my-auto">
+      <span class="spinner-border text-primary" role="status">
+      </span>
+      <p class="text-muted mt-2 small animate-flash-infinite">{{ message }}</p>
     </div>
-    <div class="text-muted mt-2 small animate-flash-infinite">{{ message }}</div>
   </div>
 </template>
 
