9e25b2023636f2acf9985fcbb25b53aeae3496a7
aendoarphin-hi <apineda@haydenindustrial.com>
2026-09-02 16:55:20 -0500

    rename

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,5 +1,5 @@
 // event types
-export const eventTypeMap = {
+export const eventTypes = {
   company: ['general', 'holiday', 'training', 'meeting'],
   employee: ['birthday', 'anniversary', 'promotion', 'new hire'],
   announcement: ['general', 'urgent', 'maintenance', 'production', 'weather', 'safety']
