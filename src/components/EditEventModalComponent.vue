de25cd0f8d21adbb65078e6abfc0ffe593b12c9f
Arhon <aronp2000@gmail.com>
2026-09-02 23:16:58 -0500

    tweak edit logic

--- src/components/EditEventModalComponent.vue
+++ src/components/EditEventModalComponent.vue
@@ -6,22 +6,22 @@
         <div class="modal-header">
           <div class="d-flex flex-column w-100 gap-1">
             <strong class="text-nowrap overflow-hidden me-4" style="text-overflow: ellipsis"
-              :title="currentEvent.title">
-              {{ currentEvent.title }}
+              :title="updatedEvent.title">
+              {{ updatedEvent.title }}
             </strong>
             <div class="d-flex flex-row gap-2 align-items-center">
               <div class="badge rounded-pill text-capitalize" style="width: min-content;"
-                :class="badgeClass(currentEvent.type)">
-                {{ formatLabel(currentEvent.type) }} |
-                {{ formatLabel(currentEvent.subtype) }}
+                :class="badgeClass(updatedEvent.type)">
+                {{ formatLabel(updatedEvent.type) }} |
+                {{ formatLabel(updatedEvent.subtype) }}
               </div>
               <small class="text-muted">
-                <span v-if="currentEvent.location">
-                  <MapMarker /> {{ currentEvent.location }}&nbsp;&nbsp;&middot;&nbsp;&nbsp;
+                <span v-if="updatedEvent.location">
+                  <MapMarker /> {{ updatedEvent.location }}&nbsp;&nbsp;&middot;&nbsp;&nbsp;
                 </span>
                 <CalendarRangeOutline />&nbsp;
-                <span v-if="currentEvent.allDay">{{ formatDate(currentEvent.start) }}</span>
-                <span v-else>{{ formatDate(currentEvent.start) }} - {{ formatDate(currentEvent.end) }}</span>
+                <span v-if="updatedEvent.allDay">{{ formatDate(updatedEvent.start) }}</span>
+                <span v-else>{{ formatDate(updatedEvent.start) }} - {{ formatDate(updatedEvent.end) }}</span>
               </small>
             </div>
           </div>
@@ -29,28 +29,31 @@
         <!-- edit mode body -->
         <div class="modal-body">
           <!-- event description -->
-          <p>
-            {{ currentEvent.description || "No description provided." }}
+          <p v-if="!editing">
+            {{ updatedEvent.description || "No description provided." }}
           </p>
+          <textarea class="form-control form-control-sm" placeholder="What is this event about?" v-else
+            v-model="updatedEvent.description" style="height: 150px; resize: none;">
+          </textarea>
           <!-- event editable fields -->
           <div class="mb-2 d-flex flex-row gap-2 w-100">
             <!-- date range -->
             <div class="w-100">
               <label for="event-edit-start-date" class="small fw-semibold">Start</label>
               <input type="datetime-local" class="form-control form-control-sm" :disabled="!editing"
-                name="event-edit-start-date" id="event-edit-start-date" v-model="startDate" />
+                name="event-edit-start-date" id="event-edit-start-date" v-model="updatedEvent.start" />
             </div>
             <div class="w-100">
               <label for="event-edit-end-date" class="small fw-semibold">End</label>
               <input type="datetime-local" class="form-control form-control-sm"
-                :disabled="!editing || currentEvent.allDay" name="event-edit-end-date" id="event-edit-end-date"
-                v-model="endDate" />
+                :disabled="!editing || updatedEvent.allDay" name="event-edit-end-date" id="event-edit-end-date"
+                v-model="updatedEvent.end" />
             </div>
           </div>
 
           <!-- location dropdown -->
           <select :disabled="!editing" id="event-edit-location-select" class="form-select form-select-sm"
-            v-model="location">
+            v-model="updatedEvent.location_id">
             <option :value="null">Select Location</option>
             <option v-for="l in locations" :key="l.name + '-' + l.id" :value="l.id">
               {{ l.name }}
@@ -67,11 +70,11 @@
         <div v-else class="modal-footer p-2">
           <button data-bs-dismiss="modal" v-if="!editing" class="btn btn-sm btn-secondary me-2"
             title="Close">Close</button>
-          <button class="btn btn-sm btn-danger me-2" @click="confirmDelete = true" title="Delete">
+          <button v-if="!editing" class="btn btn-sm btn-danger me-2" @click="confirmDelete = true" title="Delete">
             Delete
           </button>
-          <button v-if="editing" class="btn btn-sm btn-danger me-2" @click="resetChanges" title="Discard">
-            Discard
+          <button data-bs-dismiss="modal" v-if="editing" class="btn btn-sm btn-secondary me-2" title="Discard Changes">
+            Discard Changes
           </button>
           <button v-if="!editing" class="btn btn-sm btn-primary me-2" :disabled="editing" @click="startEditing()"
             title="Edit event">
@@ -94,6 +97,7 @@ import CalendarRangeOutline from "vue-material-design-icons/CalendarRangeOutline
 import MapMarker from "vue-material-design-icons/MapMarker.vue";
 import { Modal } from "bootstrap";
 import { store } from "@/common/store";
+import { toMySqlDateTime } from "@/common/helpers";
 
 export default {
   components: {
@@ -110,25 +114,16 @@ export default {
       editing: false,
       changed: false,
       confirmDelete: false,
-
-      startDate: this.event?.start ? this.formatDateTimeLocal(this.event.start) : new Date().toISOString().slice(0, 16),
-      endDate: this.event?.end ? this.formatDateTimeLocal(this.event.end) : new Date().toISOString().slice(0, 16),
-      location: this.event?.location_id || null,
-
+      updatedEvent: {},
       originalEvent: {},
-      currentEvent: {},
       locations: [],
     };
   },
-
   async mounted() {
     this.$refs.modal.addEventListener("hidden.bs.modal", () => {
       this.editing = false;
       this.changed = false;
-    });    
-
-    this.originalEvent = { ...this.event };
-    this.currentEvent = { ...this.event };
+    });
 
     // remove focus from any input fields; fix for aria warning after modal close
     const modal = document.getElementById('edit-event-modal');
@@ -142,54 +137,64 @@ export default {
   },
 
   watch: {
-    currentEvent: {
-      handler(newVal) {
-        if (JSON.stringify(newVal) !== JSON.stringify(this.originalEvent)) {
-          this.changed = true;
-        }
-      }
+    updatedEvent: {
+      handler(newValue) {
+        const updated = {
+          ...newValue,
+          start: toMySqlDateTime(newValue.start),
+          end: toMySqlDateTime(newValue.end),
+        };
+
+        const original = {
+          ...this.event,
+          start: toMySqlDateTime(this.event.start),
+          end: toMySqlDateTime(this.event.end),
+        };
+
+        this.changed = JSON.stringify(updated) !== JSON.stringify(original);
+      },
+      deep: true
     },
+
     event(newEvent) {
-      this.currentEvent = newEvent;
-      this.editing = false;
-      this.startDate = newEvent?.start ? this.formatDateTimeLocal(newEvent.start) : "";
-      this.endDate = newEvent?.end ? this.formatDateTimeLocal(newEvent.end) : "";
-      this.location = newEvent?.location_id || "";
+      this.updatedEvent = structuredClone(newEvent);
+
+      this.updatedEvent.start = this.formatDateTimeLocal(newEvent.start);
+      this.updatedEvent.end = this.formatDateTimeLocal(newEvent.end);
+
       this.changed = false;
+      this.editing = false;
     },
   },
-
   computed: {
   },
-
   methods: {
     resetChanges() {
       this.editing = false;
       this.changed = false;
-      this.startDate = this.currentEvent?.start ? this.formatDateTimeLocal(this.currentEvent.start) : "";
-      this.endDate = this.currentEvent?.end ? this.formatDateTimeLocal(this.currentEvent.end) : "";
-      this.location = this.currentEvent?.location_id || null;
+      this.updatedEvent = { ...this.event };
     },
     saveChanges() {
       try {
-        console.log('saving changes')
+        const data = {
+          ...this.updatedEvent,
+          start: toMySqlDateTime(this.updatedEvent.start),
+          end: toMySqlDateTime(this.updatedEvent.end),
+        };
+
+        console.log(data);
       } catch (error) {
         console.error('Error saving changes:', error);
       } finally {
-        // reset state
-        this.editing = false;
-        this.changed = false;
-        this.startDate = this.currentEvent?.start ? this.formatDateTimeLocal(this.currentEvent.start) : "";
-        this.endDate = this.currentEvent?.end ? this.formatDateTimeLocal(this.currentEvent.end) : "";
-        this.location = this.currentEvent?.location_id || null;
+        this.resetChanges();
       }
     },
     async deleteEvent() {
       try { // TODO: add log inserts
         await this.$axios.post(this.$api + "events?delete", {
-          id: this.currentEvent.id
+          id: this.updatedEvent.id
         });
-        this.confirmDelete = false;
+        this.resetChanges();
         store.events = (await this.$axios.get(this.$api + "events?all=1")).data; // refresh store
         Modal.getOrCreateInstance(document.getElementById('edit-event-modal')).hide();
       } catch (error) {
@@ -197,16 +202,14 @@ export default {
       }
     },
     startEditing() {
-      console.log(Object.keys(this.originalEvent));
+      this.editing = true;
     },
     formatLabel(value) {
       if (!value) return "-";
-
       return value.replace(/_/g, " ").replace(/\b\w/g, (c) => c.toUpperCase());
     },
     formatDate(value) {
       if (!value) return "-";
-
       return new Date(value).toLocaleDateString();
     },
     formatDateTimeLocal(value) {
