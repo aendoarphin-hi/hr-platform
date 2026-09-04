1887c60b3f8e05c2c1d90866eee323b9cd311b12
Arhon <aronp2000@gmail.com>
2026-09-04 01:20:59 -0500

    remove store for data

--- src/common/store.js
+++ src/common/store.js
@@ -2,12 +2,4 @@ import { reactive } from "vue";
 // global reactive store
 export let store = reactive({ // eslint-disable-line
   authenticated: false,
-  events: [],
-  employees: [],
-  screens: [],
-  playlists: [],
-  content: [],
-  approvals: [],
-  activity: [],
-  locations: [],
 });
